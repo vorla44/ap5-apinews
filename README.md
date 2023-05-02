aaf5232ed093b9026e5dbe2bd5de1361788f33f7
Veikko Rintala <veikko0912@gmail.com>
2023-05-02 13:33:06 +0300

    Commit initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# What is this project?
+This app accesses news about a partucular topic and sends it by email. 
---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+
+api_key = "4bb909ddc48448b2b40563d1626f6d12"
+
+url = "https://newsapi.org/v2/everything?q=tesla&\
+from=2023-04-02&sortBy=publishedAt&apiKey=4bb909ddc48448b2b40563d1626f6d12"
+
+#Make request
+request = requests.get(url)
+
+#Get a dictionary with data
+content = request.json()
+
+#Access the article titles and description
+for article  in content["articles"]:
+    print(article["title"])
+    print(article["description"])
