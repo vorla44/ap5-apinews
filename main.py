1cc280de5b61a51453c43de816697396cc8f5a9e
Veikko Rintala <veikko0912@gmail.com>
2023-05-02 14:03:40 +0300

    Day 27 : Send an Email with news titles and descriptions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "4bb909ddc48448b2b40563d1626f6d12"
 
@@ -12,6 +13,11 @@ request = requests.get(url)
 content = request.json()
 
 #Access the article titles and description
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message = body)
+
